3a3c4321aa0347891dcf3820471170f76c970b80
khang <khanghugo123@gmail.com>
2021-12-08 00:55:30 -0500

    weather stuff

--- script/weather.sh
+++ script/weather.sh
@@ -3,6 +3,10 @@
 # curl "https://wttr.in/Amherst?format=%C+%t\n" > ~/.dwm/weather
 while :
 do
-	curl "https://wttr.in/?format=%C+%t\n" > ~/.dwm/weather
-	sleep 30m # update every 30 minutes
+	curl "https://wttr.in/Amherst?format=%C+%t\n" > ~/.dwm/weather &
+	sleep 2m  # update every 30 minutes
+	curl "https://wttr.in/Amherst?format=%C+%t\n" > ~/.dwm/weather &
+	sleep 30  #
+	curl "https://wttr.in/Amherst?format=%C+%t\n" > ~/.dwm/weather &
+	sleep 30m  
 done
